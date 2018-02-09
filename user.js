9fbf924389972e57629367e58ffe5f74031d7fd5
Patrick Knight <ptknight00@gmail.com>
2018-02-08 20:21:20 -0600

    update

--- user.js
+++ user.js
@@ -1,35 +1,38 @@
 var weather = require('weather-js')
 var moment = require('moment')
 
+
 function UserSearch(name, location) {
     this.name = name;
     this.location = location;
     this.date = Date.now()
-    
+
     this.weather = weather.find({
-        search: this.location,
-        degreeType: 'F'},
-        function(err, result) {
-        if(err) console.log(err);
-       
-        console.log(result[0].location.name);
-        console.log(result[0].current.temperature);
-       });
-    
-    
-    // weather.find({
-    //         search: this.location,
-    //         degreeType: 'F',},
-    //         function (err, result){
-    //         if (err) throw err;
-    //         console.log(JSON.stringify(result, null, 2));
-            
-    //     })
-    
+            search: this.location,
+            degreeType: 'F'
+        },
+        function (err, result) {
+            if (err) console.log(err);
+
+            var loc = result[0].location.name;
+            var deg = result[0].current.temperature;
+            console.log(loc);
+            console.log(deg);
+
+            fs.appendFile('my-file.txt',
+                `
+            \n ${loc}
+            \n ${deg}
+            \n ${this.date}
+            `,
+                'utf8',
+                function (err, data) {
+
+                    if (err) throw err;
+                })
+        });
+
 }
 
 var test = new UserSearch('pat', 'chicago');
-// console.log(test);
-console.log(test.weather)
-
-
+console.log(test.weather);
